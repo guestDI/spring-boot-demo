90c8abec5672d44175798a913c18346ea61a603e
Name <ignatovich.dm@gmail.com>
2024-07-30 16:02:49 +0200

    lifecycle methods

--- src/main/java/com/di/java/demo/javademo/common/Boat.java
+++ src/main/java/com/di/java/demo/javademo/common/Boat.java
@@ -1,14 +1,30 @@
 package com.di.java.demo.javademo.common;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
-@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class Boat implements Vehicle{
     @Override
     public String getVehicleType() {
+        System.out.println("Method");
         return "Boat +++";
+    }
+
+    public Boat() {
+        System.out.println("Constructor");
+    }
+
+    @PostConstruct
+    public void postMount(){
+        System.out.println("Post constructor");
+    }
+
+    @PreDestroy
+    public void preUnmount(){
+        System.out.println("Pre unmount");
     }
 }
--- src/main/java/com/di/java/demo/javademo/common/Car.java
+++ src/main/java/com/di/java/demo/javademo/common/Car.java
@@ -1,8 +1,10 @@
 package com.di.java.demo.javademo.common;
 
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 @Component
+@Lazy
 public class Car implements Vehicle{
     @Override
     public String getVehicleType() {
