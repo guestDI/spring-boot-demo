4dfc8b679a14759bc3247aec7593b71dbbd0c35f
Name <ignatovich.dm@gmail.com>
2024-07-30 15:05:04 +0200

    scope

--- src/main/java/com/di/java/demo/javademo/common/Boat.java
+++ src/main/java/com/di/java/demo/javademo/common/Boat.java
@@ -1,8 +1,11 @@
 package com.di.java.demo.javademo.common;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class Boat implements Vehicle{
     @Override
     public String getVehicleType() {
--- src/main/java/com/di/java/demo/javademo/rest/VehicleController.java
+++ src/main/java/com/di/java/demo/javademo/rest/VehicleController.java
@@ -8,10 +8,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 @org.springframework.web.bind.annotation.RestController
 public class VehicleController {
     private Vehicle myVehicle;
+    private Vehicle myVehicle2;
 
     @Autowired
-    public VehicleController(@Qualifier("boat") Vehicle vehicle){
+    public VehicleController(@Qualifier("boat") Vehicle vehicle, @Qualifier("boat") Vehicle vehicle2){
         myVehicle = vehicle;
+        myVehicle2 = vehicle2;
     }
 
 //    @Autowired
@@ -22,5 +24,10 @@ public class VehicleController {
     @GetMapping("/myvehicle")
     public String printMessage(){
         return "This is " + myVehicle.getVehicleType();
+    }
+
+    @GetMapping("/check")
+    public String check(){
+        return "This is " + (myVehicle == myVehicle2);
     }
 }
